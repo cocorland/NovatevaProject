107b0777ce94bb7528179dcdb7b11ba509de6786
Orlando Chaparro <orlancss1@gmail.com>
2022-05-27 16:08:48 -0400

    .

--- src/components/MainPage/Chats.js
+++ src/components/MainPage/Chats.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Message } from '../mensajeNovateva/Message'
 
 export const Chats = () => {
   return (
-    <div>Palabra</div>
+    <div className="chatBoxColumn">
+      <div className="chatBoxWrapper">
+        <Message />
+      </div>
+      <div className="chatBoxBottom">Type a message..</div>
+    </div>
   )
 }
---
+++ src/components/mensajeNovateva/Message.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Message.css';
+
+export const Message = () => {
+  return (
+    <>
+      <div className='Message'>
+        <div className="MessageOwnerHour">Me, 10:22</div>
+        <div className="MessageText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error unde, consequatur ullam temporibus praesentium ipsam odio aliquid vel in est laboriosam, illum esse, harum soluta ratione! Distinctio expedita deserunt eaque!</div>
+      </div>
+    </>
+  )
+}
